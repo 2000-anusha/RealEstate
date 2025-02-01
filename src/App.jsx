c389504c1593e825475524fc7a1c63dd7ae060e9
Anusha Shetty <anushasshetty200@gmail.com>
2025-02-01 18:59:04 +0530

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function app() {
+  return (
+    
+    <div className="App">
+      My App
+    </div>
+  );
+}
+
+export default app;
---
+++ src/utils/accordion.jsx
@@ -0,0 +1,24 @@
+
+import { HiShieldCheck } from "react-icons/hi"; 
+import { MdCancel, MdAnalytics } from "react-icons/md";  
+const data = [
+    {
+        icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+    "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim mi"
+    }, 
+    {
+        icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+    "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim mi"
+    }, 
+    {
+        icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+    "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim mi"
+    },
+];
+    export default data;
